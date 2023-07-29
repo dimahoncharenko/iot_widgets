9fa1c32728dd0ea1c976cc35b1dde6bfdae87bbb
Dima Honcharenko <d.goncharenko.man@gmail.com>
2023-07-29 18:39:50 +0300

    Changed mock api

--- src/services/mock-api.service.ts
+++ src/services/mock-api.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class MockAPIService {
-  private baseUrl = "http://localhost:3000/api/v1/params"
+  private baseUrl = "https://iot-mock-api.onrender.com/api/v1/params"
 
   constructor(private http: HttpClient) { }
 
